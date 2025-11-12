5606ad2f528892d95f29be77509ac30b783e8d0c
Swapnil Hardade <153900991+swapnilhardade@users.noreply.github.com>
2025-11-12 17:48:46 +0530

    Refactor Graph class and update display methods
    Refactor Graph class methods and user input handling.

--- ten.py
+++ ten.py
@@ -1,56 +1,52 @@
-# Define a class for the Graph
 class Graph:
     def _init_(self):
-        # Dictionary to store adjacency list
         self.graph = {}
 
-    # Function to add a vertex
-    def add_vertex(self, vertex):
-        if vertex not in self.graph:
-            self.graph[vertex] = []
+    def add_vertex(self, v):
+        if v not in self.graph:
+            self.graph[v] = []
 
-    # Function to add an edge (undirected graph)
-    def add_edge(self, vertex1, vertex2):
-        if vertex1 in self.graph and vertex2 in self.graph:
-            self.graph[vertex1].append(vertex2)
-            self.graph[vertex2].append(vertex1)
-        else:
-            print("Error: One or both vertices not found!")
+    def add_edge(self, u, v):
+        if u in self.graph and v in self.graph:
+            self.graph[u].append(v)
+            self.graph[v].append(u)
 
-    # Display the graph as adjacency list
-    def display_adj_list(self):
-        print("\nGraph represented as an Adjacency List:")
-        for vertex in self.graph:
-            print(vertex, "->", " -> ".join(map(str, self.graph[vertex])))
+    def display_list(self):
+        print("\nGraph using Adjacency List:")
+        for v in self.graph:
+            print(v, "->", self.graph[v])
 
-    # Display the graph as adjacency matrix
-    def display_adj_matrix(self):
-        print("\nGraph represented as an Adjacency Matrix:")
+    def display_matrix(self):
         vertices = list(self.graph.keys())
-        print("   ", "  ".join(vertices))
-        for v1 in vertices:
-            row = []
-            for v2 in vertices:
-                if v2 in self.graph[v1]:
-                    row.append("1")
+        print("\nGraph using Adjacency Matrix:")
+        print("   ", end="")
+        for v in vertices:
+            print(v, end=" ")
+        print()
+        for i in vertices:
+            print(i, end="  ")
+            for j in vertices:
+                if j in self.graph[i]:
+                    print("1", end=" ")
                 else:
-                    row.append("0")
-            print(v1, "  ", "  ".join(row))
+                    print("0", end=" ")
+            print()
 
-
-# ---- Main Program ----
 g = Graph()
+v = int(input("Enter number of vertices: "))
+for i in range(v):
+    vertex = input(f"Enter vertex {i+1}: ")
+    g.add_vertex(vertex)
 
-# Insert vertices
-vertices = ['A', 'B', 'C', 'D', 'E']
-for v in vertices:
-    g.add_vertex(v)
+e = int(input("Enter number of edges: "))
+for i in range(e):
+    u = input("Enter first vertex: ")
+    v = input("Enter second vertex: ")
+    g.add_edge(u, v)
 
-# Insert edges
-edges = [('A', 'B'), ('A', 'C'), ('B', 'D'), ('C', 'D'), ('D', 'E')]
-for v1, v2 in edges:
-    g.add_edge(v1, v2)
-
-# ---- Choose one display operation below ----
-g.display_adj_list()     # Display using adjacency list
-# g.display_adj_matrix() # Display using adjacency matrix
+print("\n1. Display Adjacency List\n2. Display Adjacency Matrix")
+choice = int(input("Enter your choice (1 or 2): "))
+if choice == 1:
+    g.display_list()
+else:
+    g.display_matrix()
